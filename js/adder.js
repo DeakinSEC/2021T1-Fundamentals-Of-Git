a94af3e72a3626123fe8b71965e61581cc8a6dbe
Ravindu Liyanapathirana <rliyanapathiran@deakin.edu.au>
2021-03-26 15:32:37 +1100

    Add sakura.css, show confetti upon addition

--- js/adder.js
+++ js/adder.js
@@ -14,5 +14,24 @@
 
 	submitButton.onclick = () => {
 		resultSpan.innerText = n1Input.valueAsNumber + n2Input.valueAsNumber;
+
+		// Show confetti 🎉!
+		const submitButtonBounds = submitButton.getBoundingClientRect();
+		confetti({
+			// Physics refinements.
+			particleCount: 75,
+			startVelocity: 35,
+			gravity: 1.75,
+			spread: 25,
+
+			// DSEC colors!
+			colors: ['#b03574', '#b03574', '#4ff1e4', '#009999'],
+
+			// Originate at the center of the submit button.
+			origin: {
+				x: ((submitButtonBounds.left + submitButtonBounds.right) / 2) / window.innerWidth,
+				y: ((submitButtonBounds.top + submitButtonBounds.bottom) / 2) / window.innerHeight,
+			},
+		});
 	};
 })();
