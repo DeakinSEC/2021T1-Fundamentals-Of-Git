bb5bbab5ee75ade232b7cf45701e93e1b8b29413
Ravindu Liyanapathirana <rliyanapathiran@deakin.edu.au>
2021-03-26 14:43:34 +1100

    Fix adder logic
    Uses HTMLInputElement.valueAsNumber for the calculation instead of
    adding the HTMLInputElements themselves (which causes the result to be
    '[object HTMLInputElement][object HTMLInputElement]')

--- js/adder.js
+++ js/adder.js
@@ -1,10 +1,18 @@
 (() => {
+
+	/** @type HTMLInputElement */
 	const n1Input = document.getElementById('adder-n1');
+
+	/** @type HTMLInputElement */
 	const n2Input = document.getElementById('adder-n2');
+
+	/** @type HTMLButtonElement */
 	const submitButton = document.getElementById('adder-submit');
+
+	/** @type HTMLSpanElement */
 	const resultSpan = document.getElementById('adder-result');
 
 	submitButton.onclick = () => {
-		resultSpan.innerText = n1Input + n2Input;
+		resultSpan.innerText = n1Input.valueAsNumber + n2Input.valueAsNumber;
 	};
 })();
